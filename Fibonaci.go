8176b2393cc4a47dcdfa19b6092e3f88ac6e2aa2
akbar <akbarraw09@gmail.com>
2022-10-14 09:53:20 +0700

    pembaruan fibonaci

--- Fibonaci.go
+++ Fibonaci.go
@@ -2,14 +2,16 @@ package fibonaci
 
 func Fibonaci(nilai int)[]int{
 	var tamp = 1
+	var tamp2 = 0
 	var angka []int
 	for i := 0; i<nilai; i++{
 
 		if i % 2 == 0{
-			angka = append(angka, tamp)
-			tamp += i
+			angka = append(angka, tamp2)
+			tamp2 += tamp
 		} else{
-			angka = append(angka, i)
+			angka = append(angka, tamp)
+			tamp += tamp2
 		}
 	}
 	return angka
